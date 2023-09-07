9768864d86cb28a41d999685294906a9ee32cae0
leen <leenodeh287@gmail.com>
2023-09-07 21:04:59 +0300

    update grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      sass: {
+        options: {
+          sourceMap: true, // Generate source maps
+          outputStyle: 'compressed', // Set the output style (nested, expanded, compact, compressed)
+        },
+        dist: {
+          files: {
+            'css/styles.css': 'sass/styles.scss' // Output file: Input file
+          }
+        }
+      },
+      watch: {
+        options: {
+          livereload: 12345 // Change the port number to a different one
+        },
+        sass: {
+          files: ['sass/**/*.scss'],
+          tasks: ['sass'],
+        },
+      },
+      
+    });
+  
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+  
+    grunt.registerTask('default', ['watch']); // Default task to watch for changes
+  };
+  
