211a8f990ed81a1ba28eba6e4737e1f40ecd86fd
leen <leenodeh287@gmail.com>
2023-09-10 23:36:10 +0300

    no response js-file :(

---
+++ Club-Monaco-Task/js/main.js
@@ -0,0 +1,10 @@
+const sec=document.getElementsByName("section");
+var divElement = document.getElementsByName("col-lg-4");
+
+function showFilters() {
+    var filters = document.getElementById("filters");
+    filters.style.display = (filters.style.display === "none" || filters.style.display === "") ? "block" : "none";
+  
+}
+
+
