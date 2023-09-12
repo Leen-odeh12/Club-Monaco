f04a4defb9a0bce345f1be2c6c459bcb7823deb1
leen <leenodeh287@gmail.com>
2023-09-12 13:25:02 +0300

    add show-filter functionality

--- Club-Monaco-Task/js/main.js
+++ Club-Monaco-Task/js/main.js
@@ -1,10 +1,26 @@
 const sec=document.getElementsByName("section");
 var divElement = document.getElementsByName("col-lg-4");
+var elements = document.querySelectorAll('.col-lg-4');
+
+var isVisible = true;
 
 function showFilters() {
     var filters = document.getElementById("filters");
     filters.style.display = (filters.style.display === "none" || filters.style.display === "") ? "block" : "none";
-  
+    for (var i = 0; i < elements.length; i++) {
+    if (isVisible) {
+      elements[i].classList.replace('col-lg-4', 'col-lg-3');
+    } else {
+      elements[i].classList.replace('col-lg-3', 'col-lg-4');
+    }
+  }
+  isVisible = !isVisible;
 }
+
+
+
+
+var showFiltersButton = document.querySelector('.btn');
+showFiltersButton.addEventListener('click', showFilters);
 
 
